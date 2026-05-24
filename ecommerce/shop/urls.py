7afc7dbb6a8c22a8f202d4162fa2f96400f4364c
Subho44 <98902921+Subho44@users.noreply.github.com>
2026-05-24 10:26:55 +0530

    ok
    ok

--- ecommerce/shop/urls.py
+++ ecommerce/shop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from.views import home,add_product
 
 urlpatterns = [
-    path('',home);
+    path('',home, name='home'),
+    path('add-product/',add_product,name='add_product'),
 ]
--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+default_products = [
+    {
+        'name': "Laptop",
+        'price': 1299,
+        'category': "Electronics",
+        'rating': 4.5,
+        'image': "https://picsum.photos/seed/laptop/400/300"
+    },
+    {
+        'name': "Wireless Headphone",
+        'price': 999,
+        'category': "Electronics",
+        'rating': 4.2,
+        'image': "https://picsum.photos/seed/headphone/400/300"
+    }
+]
+
+def home(request):
+    session_products = request.session.get('products', [])
+    products = default_products + session_products
+
+    return render(request, "home.html", {
+        'products': products
+    })
+
+def add_product(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        price = request.POST.get('price')
+        category = request.POST.get('category')
+        rating = request.POST.get('rating')
+        image = request.POST.get('image')
+
+        new_product = {
+            'name': name,
+            'price': price,
+            'category': category,
+            'rating': rating,
+            'image': image
+        }
+
+        session_products = request.session.get('products', [])
+        session_products.append(new_product)
+
+        request.session['products'] = session_products
+        request.session.modified = True
+
+        return redirect('home')
+
+    return render(request, 'add_product.html')
